000d3bd874cf489e12e97fbb0c0ff95ca4f6c092
Nerea <jguillen.nereajimenez@gmail.com>
2019-11-26 17:37:15 +0100

    express session

--- Practicas/plantilla.js
+++ Practicas/plantilla.js
@@ -1,18 +1,28 @@
 'use strict'
+//instalar ejs, express, body-parser, cookie-parser, express-session
 const express = require("express");
 const path = require("path");
 const app = express();
-const fs = require("fs");//para read file
+const fs = require("fs"); //para read file
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 
-//instalar ejs, express, body-parser, cookie-parser
+
+const middlewareSession = session({
+    saveUninitialized: false,
+    secret: "foobar34",
+    resave: false
+});
+
 
 app.listen(3000, function (err) {
     if (err) {
