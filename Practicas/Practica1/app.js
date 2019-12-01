1859b9d3455eb16b346f208ef9f1d2c93365a76d
Yhondri <yhondri@gmail.com>
2019-12-01 17:46:51 +0100

    Arregla problemas de merge.

--- Practicas/Practica1/app.js
+++ Practicas/Practica1/app.js
@@ -1,44 +1,46 @@
 'use strict'
-//instalar ejs, express, body-parser, cookie-parser, express-session
+
+/* Ficheros del proyecto (creados por nosotros)*/
+const config = require("./config");
+
+/* DAOs */
+const userDAO = require("./userDAO");
+
+/* Frameworks */
+const mysql = require("mysql");
+const pool = mysql.createPool(config.mysqlConfig); // Crear un pool de conexiones a la base de datos de MySQL
 const express = require("express");
-const path = require("path");
 const app = express();
+const path = require("path");
 const fs = require("fs"); //para read file
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const session = require("express-session");
-const mysqlSession = require("express-mysql-session");
-const MySQLStore = mysqlSession(session);
-const sessionStore = new MySQLStore({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "miBD"
-});
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+/* setup */
+app.set("view engine", "ejs"); //configurar ejs como motor de plantillas
+app.set("views", path.join(__dirname, "views")); //directorio donde van a estas las vistas plantillas
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
 
-const middlewareSession = session({
-    saveUninitialized: false,
-    secret: "foobar34",
-    resave: false,
-    store: sessionStore
+/* POST - Sección para implementar las peticiones POST */
+
+
+/* GET - Sección para implementar las peticiones GET */
+
+app.get("/", function(request, response) {
+    response.statusCode = 200;
+    response.type("text/plain; charset=utf-8");
+    response.redirect("/newUser.html")
 });
 
-app.use(middlewareSession);
-
-
-app.listen(3000, function (err) {
+/* Listener */
+app.listen(3000, function(err) {
     if (err) {
-        console.error("No se pudo inicializar el servidor: " +
-            err.message);
+        console.error("No se pudo inicializar el servidor: " + err.message);
     } else {
-        console.log("Servidor arrancado en el puerto 3000");
+        console.log(`Servidor arrancado en el puerto ${config.port}`);
     }
 });
