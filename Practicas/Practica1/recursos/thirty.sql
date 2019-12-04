97855759aff32ab37b87b5c4922bdfd665861fd7
Nerea <jguillen.nereajimenez@gmail.com>
2019-12-04 19:19:16 +0100

    bbdd

--- Practicas/Practica1/recursos/thirty.sql
+++ Practicas/Practica1/recursos/thirty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-12-2019 a las 18:06:24
+-- Tiempo de generación: 04-12-2019 a las 19:18:48
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.4
 
@@ -42,6 +42,53 @@ INSERT INTO `amigos` (`emailAmigo1`, `emailAmigo2`, `amigos`) VALUES
 ('neku@ucm.es', 'ejemplo@gmail.com', 1),
 ('neku@ucm.es', 'wilson@gmail.com', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
+('1b5mDuyjNVdB4Kbc6rqhm6oBggr7rh2o', 1575477786, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"ejemplo@gmail.com\"}'),
+('jmHAhOQ6cBVpIJAKSunTRErkhpdvwPd0', 1575568253, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"neku@ucm.es\"}'),
+('lkgqattiathe1t1e6AUaEgcICT_of0Bn', 1575549004, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"neku@ucm.es\"}');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `email` varchar(200) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `birthDate` date DEFAULT NULL,
+  `img` mediumtext,
+  `puntos` int(255) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`email`, `password`, `name`, `gender`, `birthDate`, `img`, `puntos`) VALUES
+('ejemplo@gmail.com', '123', 'Ejemplo', 'F', '2001-05-01', NULL, 0),
+('neku@ucm.es', 'aaa', 'Neku', 'F', '1997-06-24', 'b32a2d7655b0414482f1c18704a43e51', 0),
+('wilson@gmail.com', 'asd', 'Wilson', 'F', '1999-07-16', NULL, 0),
+('yhon@ucm.es', '234', 'Yhon', 'M', '1994-02-01', 'd7cfb42d3bb9dd3be5e7fbbeca380788', 0);
+
 --
 -- Índices para tablas volcadas
 --
@@ -53,6 +100,18 @@ ALTER TABLE `amigos`
   ADD PRIMARY KEY (`emailAmigo1`,`emailAmigo2`),
   ADD KEY `emailAmigo2_fk2` (`emailAmigo2`) USING BTREE,
   ADD KEY `emailAmigo1_fk1` (`emailAmigo1`) USING BTREE;
+
+--
+-- Indices de la tabla `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`email`);
 
 --
 -- Restricciones para tablas volcadas
